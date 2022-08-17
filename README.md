ce01d25add218c8b5049407e3ff9866874e0fb93
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-08-17 17:41:43 -0300

    Finalização da Aula 1 e suas atividades

---
+++ README.md
@@ -0,0 +1,17 @@
+# JavaScript  Orientado a Objetos
+
+
+
+## Projetos desenvolvidos durante o curso de JavaScript Orientado a Objetos, da Alura.
+
+
+
+## O quê eu aprendi?
+
+### Na Aula 1:
+
+- Criação de classes;
+- Conceito de Atributos;
+- Instância e Objeto;
+- Operador new;
+
--- aula-01/index.js
+++ aula-01/index.js
@@ -1,11 +1,8 @@
-const cliente1Nome = 'Ricardo';
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
 
-const cliente2Nome = 'Alice';
-const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
-
-console.log(cliente1Nome);
+const cliente1 = new Cliente();
